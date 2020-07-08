79d140fe94253984fea33e34204a03bed25867fd
Ziyad AlYafi <ziyadalyafi@Ziyads-MBP.lan>
2020-07-08 10:59:36 -0400

    carta-mac: unignore custom .zsh files

---
+++ custom/env_carta-mac.zsh
@@ -0,0 +1,3 @@
+# Environment variables
+export COMPOSE_DOCKER_CLI_BUILD=1 # improves cache hits by using the CLI docker for building
+export DOCKER_BUILDKIT=1 # improves build speed by using docker's buildkit engine
