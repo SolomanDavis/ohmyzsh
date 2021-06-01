580242914c77d2517dda186956cf5cbc81b070e9
Ziyad AlYafi <ziyadalyafi@Ziyads-MBP.lan>
2021-06-01 14:53:26 -0400

    Remove old files

--- custom/dotfiles_carta-mac.zsh
+++
@@ -1,6 +0,0 @@
-# Script to make dotfiles git config usable
-# Idea taken from: https://news.ycombinator.com/item?id=11070797
-
-# Set up alias config
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-config config status.showUntrackedFiles no
--- custom/init_carta-mac.zsh
+++
@@ -1,6 +0,0 @@
-# Terminal initiation scripts
-
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-fi
